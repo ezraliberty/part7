bdb38e2eaabdd502667f5cfea8fc3f282f414a32
ezraliberty <ezraliberty99@gmail.com>
2024-09-02 19:44:07 +0100

    refactor

--- blog-list/bloglist-frontend/src/main.jsx
+++ blog-list/bloglist-frontend/src/main.jsx
@@ -18,34 +18,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </UserContextProvider>
   </QueryClientProvider>
 )
-
-// store.dispatch(fetchBlogs)
-
-// const root = createRoot(document.getElementById('root'))
-
-// root.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>
-// )
-
-// import ReactDOM from 'react-dom/client'
-// import { createStore, applyMiddleware } from 'redux'
-// import thunkMiddleware from 'redux-thunk'
-// import { Provider } from 'react-redux'
-// import { composeWithDevTools } from 'redux-devtools/extension'
-
-// import App from './App'
-// import bloglistReducer from './reducers/notifyReducer'
-// import './index.css'
-
-// const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
-// const store = createStore(bloglistReducer, composedEnhancer)
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// )
